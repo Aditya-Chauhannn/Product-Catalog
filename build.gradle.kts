908b7ea4f8769683ca339756b5b3c377b5cc1ef3
Aditya-Chauhannn <159634302+Aditya-Chauhannn@users.noreply.github.com>
2025-04-09 22:22:42 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,129 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-val compose_version = "1.5.10"
-val kotlin_version = "1.9.22"
-val navigation_version = "2.7.6"
-val coroutines_version = "1.7.3"
-val lifecycle_version = "2.7.0"
-val koin_version = "3.4.3"
-val retrofit_version = "2.9.0"
-val okhttp_version = "4.11.0"
-val coil_version = "2.4.0"
-val accompanist_version = "0.30.1"
+import com.android.build.api.dsl.Packaging
 
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+var compose_version = "1.5.10"
+var kotlin_version = "1.9.22"
+var coroutines_version = "1.7.3"
+var lifecycle_version = "2.7.0"
+var koin_version = "3.5.0"
+var retrofit_version = "2.9.0"
+var okhttp_version = "4.12.0"
+var coil_version = "2.5.0"
+var accompanist_version = "0.32.0"
+var navigation_version = "2.7.7"
+
+android {
+    compileSdk = 35
+    namespace = "com.example.productcatalog"
+
+    defaultConfig {
+        applicationId = "com.example.productcatalog"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = compose_version
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.litert.support.api)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Compose
+    // Compose
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.5.10")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.10") // <-- Add this
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.10")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.10")
+
+
+
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:$navigation_version")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycle_version")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+
+    // Koin DI
+    implementation("io.insert-koin:koin-android:$koin_version")
+    implementation("io.insert-koin:koin-androidx-compose:$koin_version")
+
+    // Networking
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.okhttp3:okhttp:$okhttp_version")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttp_version")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:$coil_version")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-pager:$accompanist_version")
+    implementation("com.google.accompanist:accompanist-pager-indicators:$accompanist_version")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanist_version")
+    implementation("com.google.accompanist:accompanist-swiperefresh:$accompanist_version")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutines_version")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$compose_version")
 }
